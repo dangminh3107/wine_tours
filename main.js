d79d028abdb4bac71bc915846ed07ab1f38aa529
ozoraplus <navy31799@gmail.com>
2021-12-04 21:52:33 +0700

    add navigation

---
+++ main.js
@@ -0,0 +1,93 @@
+const user = document.querySelector('.user');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.form')
+const checkRadio = document.querySelector('.checkmark')
+const inputRadio = document.querySelector('.check-save-info')
+const tabItem = Array.from(document.querySelectorAll('.form-tab-item'))
+const tabContent = Array.from(document.querySelectorAll('.form-tab-content'))
+const socialBtn = document.querySelector('.sign-in-other-btn')
+const navItems = Array.from(document.querySelectorAll('.head-body-navigation-item'))
+const navSubItems = Array.from(document.querySelectorAll('.head-body-navigation-list'))
+const language = document.querySelector('.language')
+const megaMenu = document.querySelector('.mega-menu')
+const subMenuItem = Array.from(document.querySelectorAll('.head-body-navigation-item-sub-page'))
+
+user.onclick = function() {
+    if (!modal.classList.contains('active')) {
+        modal.style.display = 'flex'
+    }
+    modal.classList.toggle('active')
+}
+
+modal.onclick = () => {
+    if (modal.classList.contains('active')) {
+        setTimeout(() => {modal.style.display = 'none'}, 300)
+    }
+    modal.classList.toggle('active')
+}
+modalContent.onclick = (e) => {e.stopPropagation()}
+
+checkRadio.onclick = () => {
+    inputRadio.checked = !inputRadio.checked;
+}
+
+tabItem.forEach((item, index) => {
+    item.onclick = () => {
+        tabContent.forEach((tab, idx) => {
+            if (idx !== index) {
+                tab.classList.remove('active')
+            }
+        })
+        tabItem.forEach((tab, idx) => {
+            if (idx !== index) tab.classList.remove('active');
+        })
+        socialBtn.style.display = index > 0 ? 'none' : 'flex'
+        item.classList.add('active');
+        tabContent[index].classList.add('active');
+    }
+})
+
+language.onmouseover = () => {
+    language.classList.add('active');
+}
+
+language.onmouseout = () => {
+    language.classList.remove('active');
+}
+
+navItems.forEach((navItem, index) => {
+    navItem.onmouseover = () => {
+        if (index !== 6) {
+            navSubItems[index].classList.add('active');
+        }
+    }
+    navItem.onmouseout = () => {
+        if (index !== 6) {
+            navSubItems[index].classList.remove('active');
+        }
+    }
+    if (index === 6) {
+        navItem.onmouseover = () => {
+            megaMenu.classList.add('active');
+        }
+        navItem.onmouseleave = () => {
+            megaMenu.classList.remove('active');
+        }
+
+        megaMenu.onmouseover = () => {
+            megaMenu.classList.add('active');
+        }
+
+        megaMenu.onmouseleave = () => {
+            megaMenu.classList.remove('active');
+        }
+    }
+})
+
+window.onresize = () => {
+    if (document.documentElement.clientWidth>= 1024 && document.documentElement.clientWidth < 1375) {
+        subMenuItem.forEach(item => {
+            item.style.left = '-100%'
+        })
+    }
+}
